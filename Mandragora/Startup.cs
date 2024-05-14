11eaddc023a5a4d27641c28f14adbe2d484c21e2
Nurbolat Absagat <n.absagat@gmail.com>
2024-05-14 13:45:01 +0500

    Add project files.

---
+++ Mandragora/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mandragora.Startup))]
+namespace Mandragora
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
